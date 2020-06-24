52f29e1ff357fb8ff9662fbf6c05cda4c82dd821
BauerJustin <justin.bauer@mail.utoronto.ca>
2020-06-24 15:23:58 -0600

    Added comments

--- chat_bot.py
+++ chat_bot.py
@@ -33,18 +33,19 @@ def get_article(url):
 def greeting_response(text, sentences):
     text = text.lower()
 
+    # loop through all the words in the input and if any match to greeting bot will reply with a greeting
     for word in text.split():
         if word in user_greetings:
             return random.choice(bot_greetings)
 
-def index_sort(score_list):
-    length = len(score_list)
-    list_index = list(range(0,length))
+def index_sort(similarity_scores_list):
+    # create a list of indexes for each line in article
+    list_index = list(range(0,len(similarity_scores_list)))
 
-    x = score_list
-    for i in range(length):
-        for j in range(length):
-            if x[list_index[i]] > x[list_index[j]]:
+    # sort list of article indexes from highest to lowest to user input
+    for i in range(len(similarity_scores_list)):
+        for j in range(len(similarity_scores_list)):
+            if similarity_scores_list[list_index[i]] > similarity_scores_list[list_index[j]]:
                 temp = list_index[i]
                 list_index[i] = list_index[j]
                 list_index[j] = temp
@@ -52,6 +53,7 @@ def index_sort(score_list):
     return list_index
 
 def bot_article_response(user_input, sentences):
+    # score lines from article with relation to user input
     user_input = user_input.lower()
     sentences.append(user_input)
     bot_response = ""
@@ -62,15 +64,18 @@ def bot_article_response(user_input, sentences):
     index = index[1:]
     response_flag = 0
 
+    # add sentences to bot response which have the highest match to the user input
     j = 0
     for i in range(len(index)):
         if similarity_scores_list[index[i]] > 0.0:
             bot_response = bot_response+' '+sentences[index[i]]
             response_flag = 1
             j += 1
+        # limit number of matches in output
         if j > 2:
             break
 
+    # if there is not match output random message stating the bot does not know
     if response_flag == 0:
         bot_response = bot_response+' '+random.choice(bot_unknown)
 
@@ -79,6 +84,7 @@ def bot_article_response(user_input, sentences):
     return bot_response
 
 def bot_print(text):
+    # print bot output with 'typing' animation 
     text += "\n"
     sys.stdout.write("Bot: ")
     sys.stdout.flush()
@@ -88,6 +94,7 @@ def bot_print(text):
         sys.stdout.flush()
 
 def main():
+    #variables
     sentence_list = []
     article_given = False
 
@@ -95,24 +102,32 @@ def main():
     
     while(True):
 
+        # main user input
         user_input = input()
 
-        if len(user_input) > 1:
+        # user must input non whitespace characters to get a response
+        if len(user_input.strip()) > 0:
+            # Case when user says bye
             if user_input.lower() in user_bye:
                 bot_print(random.choice(bot_bye))
                 break
+            # Case when user says hello
             elif greeting_response(user_input, sentence_list) != None:
                 bot_print(greeting_response(user_input, sentence_list))
+            # Case when user is done with the article
             elif user_input == "done" and article_given:
                 sentence_list = []
                 article_given = False
                 bot_print("Article is no longer loaded.")
+            # Case when user is asking questions about the article
             elif article_given:
                 bot_print(bot_article_response(user_input, sentence_list))
+            # Case when user is inputting an article
             elif len(user_input) > 50 and not (' ' in user_input.strip()):
                 sentence_list = get_article(user_input.strip())
                 article_given = True
                 bot_print("Article loaded! Ask me your questions. \nType done when you don't have any more questions.")
+            # Case when program does not recongize users input
             else:
                 bot_print(random.choice(bot_unknown))
 
