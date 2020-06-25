bb6778022449e1240fd266dc57980a0e6381d2a0
BauerJustin <justin.bauer@mail.utoronto.ca>
2020-06-25 13:34:00 -0600

    Change code so more types of text recognition can be easily added

--- main.py
+++ main.py
@@ -11,13 +11,48 @@ file = open("text.json")
 text = json.loads(file.read())
 file.close()
     
-def greeting_response(greeting, sentences):
-    greeting = greeting.lower()
+def bot_response(sentence):
+    sentence = sentence.lower()
+    scores = [0,0,0]
 
-    # loop through all the words in the input and if any match to greeting bot will reply with a greeting
-    for word in greeting.split():
+    # loop through all the words in the input and count the matches
+    for word in sentence.split():
         if word in text['greeting'][0]:
-            return random.choice(text['greeting'][1])
+            scores[0] += 1
+        elif word in text['farewell'][0]:
+            scores[1] += 1
+    
+    max_index = len(scores)-1
+    for i in range(max_index):
+        if scores[i] > scores[max_index]:
+            max_index = i
+
+    if max_index == 0:
+        bot_print(random.choice(text['greeting'][1]))
+    elif max_index == 1:
+        bot_print(random.choice(text['farewell'][1]))
+    else:
+        bot_print(random.choice(text['unknown'][1]))
+    
+    return max_index
+
+def process_article(user_input):
+    # load article
+    sentence_list = artbot.get_article(user_input.strip())
+    article_given = True
+    bot_print("Article loaded! Ask me your questions. \nType done when you don't have any more questions.")
+    while(article_given):
+        # let user ask questions about article
+        user_input_article = input()
+        if len(user_input_article.strip()) > 0:
+            if user_input_article == "done":
+                sentence_list = []
+                bot_print("Article is no longer loaded.")
+                article_given = False
+            elif user_input_article.lower() in text['farewell'][0]:
+                bot_print("Type done first if you want to say goodbye.")
+            else:
+                bot_print(artbot.bot_article_response(user_input_article, sentence_list, text))
 
 def bot_print(sentence):
     # print bot output with 'typing' animation 
@@ -30,9 +65,6 @@ def bot_print(sentence):
         sys.stdout.flush()
 
 def main():
-    #variables
-    sentence_list = []
-    article_given = False
 
     bot_print("Hello There! I am your friendly chat bot.")
     
@@ -43,28 +75,13 @@ def main():
 
         # user must input non whitespace characters to get a response
         if len(user_input.strip()) > 0:
-            # Case when user says bye
-            if user_input.lower() in text['farewell'][0]:
-                bot_print(random.choice(text['farewell'][1]))
-                break
-            # Case when user says hello
-            elif greeting_response(user_input, sentence_list) != None:
-                bot_print(greeting_response(user_input, sentence_list))
-            # Case when user is done with the article
-            elif user_input == "done" and article_given:
-                sentence_list = []
-                article_given = False
-                bot_print("Article is no longer loaded.")
-            # Case when user is asking questions about the article
-            elif article_given:
-                bot_print(artbot.bot_article_response(user_input, sentence_list, text))
-            # Case when user is inputting an article
-            elif len(user_input) > 50 and not (' ' in user_input.strip()):
-                sentence_list = artbot.get_article(user_input.strip())
-                article_given = True
-                bot_print("Article loaded! Ask me your questions. \nType done when you don't have any more questions.")
-            # Case when program does not recongize users input
+            # Case when user inputs an article
+            if artbot.is_article(user_input):
+                process_article(user_input)
+            # Case when algorithm determines bot response
             else:
-                bot_print(random.choice(text['unknown'][1]))
+                index = bot_response(user_input)
+                if index == 1:
+                    break
 
 main()
